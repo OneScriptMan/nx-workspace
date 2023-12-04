602bcf7f9bde579261ddd608f15a3551f6c42453
OneScriptMan <vladzam79@gmail.com>
2023-12-04 13:51:32 +0300

    fix: path for data access lib

--- libs/data-access/src/index.ts
+++ libs/data-access/src/index.ts
@@ -1,5 +1,5 @@
 // export * from './lib/lib.routes';
 
-export * from './lib/data-access/services/user-api-service/user-api-service.service';
+export * from './lib/services/user-api-service/user-api-service.service';
 
-export * from './lib/data-access/services/user-api-service/iusers';
+export * from './lib/services/user-api-service/iusers';
