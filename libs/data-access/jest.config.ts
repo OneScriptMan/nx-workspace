7c88dc6d822fe6a508fa8b4b783326bbe24c4771
OneScriptMan <vladzam79@gmail.com>
2023-12-04 11:35:23 +0300

    fix: move library data-access

--- libs/data-access/jest.config.ts
+++ libs/data-access/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
   displayName: 'data-access',
-  preset: '../../../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../../coverage/libs/users/data-access',
+  coverageDirectory: '../../coverage/libs/data-access',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
